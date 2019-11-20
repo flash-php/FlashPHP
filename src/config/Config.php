be1f2681a333c1dbc133b392fcc49bad0d9fdb95
Ingo Andelhofs <ingom2000@gmail.com>
2019-11-20 18:30:42 +0100

    Added a config class that stores all project configurations

---
+++ src/config/Config.php
@@ -0,0 +1,12 @@
+<?php
+
+
+namespace FlashPHP\config;
+
+
+class Config {
+  // ROOT PATH
+  public static $DIR = __DIR__;
+  public static $HOME = __DIR__;
+  public static $HOME_PATH = __DIR__;
+}
