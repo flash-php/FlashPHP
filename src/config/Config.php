f731406cf50f5dcb943a57ea2625baf96a5bea28
Ingo Andelhofs <ingom2000@gmail.com>
2019-12-01 14:08:43 +0100

    Updated the Config file for the http/Router

--- src/config/Config.php
+++ src/config/Config.php
@@ -4,9 +4,18 @@
 namespace FlashPHP\config;
 
 
+
+/**
+ * Class Config
+ * @author Ingo Andelhofs
+ * @package FlashPHP\config
+ */
 class Config {
-  // ROOT PATH
-  public static $DIR = __DIR__;
-  public static $HOME = __DIR__;
-  public static $HOME_PATH = __DIR__;
+  // Root Path
+  public static string $DIR = __DIR__;
+  public static string $HOME = __DIR__;
+  public static string $HOME_PATH = __DIR__;
+
+  // Router
+  public static string $ROUTER_URL_PARAMETER = 'route';
 }
