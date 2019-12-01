691292ba00997def13ca5d4c8de8e7fb291e4fc9
Ingo Andelhofs <ingom2000@gmail.com>
2019-12-01 23:02:49 +0100

    Added support for middleware

---
+++ src/core/middleware/Middleware.php
@@ -0,0 +1,16 @@
+<?php
+
+
+namespace FlashPHP\core\middleware;
+
+
+
+/**
+ * Class Middleware
+ * @author Ingo Andelhofs
+ * @package FlashPHP\core\middleware
+ */
+class Middleware {
+  public static bool $next = true;
+  public static bool $block = false;
+}
--- test/index.php
+++ test/index.php
@@ -1,6 +1,10 @@
 <?php
 
+use FlashPHP\core\http\request\Request;
+use FlashPHP\core\http\response\Response;
 use FlashPHP\core\http\Router;
+use FlashPHP\core\middleware\Middleware;
+
 require_once '../vendor/autoload.php';
 
 
@@ -12,8 +16,9 @@ error_reporting (E_ALL | E_STRICT);
 
 // Routes
 $home = new Router("home");
-$home->simple_get("index", ["test", "lol"], [], function () {
-  echo "test";
+$home->simple_get("index", ["test", "lol"], [function() { return Middleware::$block; }], function (Request $req, Response $res) {
+  $res->send_r( $req->params );
+
 });
 
 
